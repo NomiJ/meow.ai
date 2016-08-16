be152618c9375186099c1b07e8e7f86b5670ddeb
Nauman Qazi <nauman.qazi@systematicbytes.com>
2016-08-17 04:11:24 +1000

    Fixed server, view authorize page and get token

--- src/routes/authorize.ts
+++ src/routes/authorize.ts
@@ -1,4 +1,5 @@
 import { Controller, Request, get } from 'vio';
+import {Authorize} from '../authorize'
 
 export default class extends Controller {
     // http://localhost:1337/authorize/token/XXXXX
@@ -6,10 +7,17 @@ export default class extends Controller {
         path: 'token/:param?'
     })
     authorize(req: Request<any>) {
-        return {
-            title: 'Hello, World!',
-            text: 'hello! thank you! thank you very much!',
-            param: req.params['token'] || 'empty'
-        };
+        let authDB = new Authorize();
+        if (authDB.getUser(req.params['param'])) {
+            let user = authDB.findUserByToken(req.params['param']);
+
+            return {
+                title: 'Meow.Authorize!',
+                text: 'Hello ' + user.getName(),
+                param: req.params['param'] || 'empty'
+            };
+        }
+
     }
+
 }
--- src/server.ts
+++ src/server.ts
@@ -25,30 +25,12 @@ let app=express();
 app.engine('hbs', handlebars);
 app.use('/static', express.static(Path.join(__dirname, '../static')));
 
-let router = new Router(app, {
+let router = new Router(app as any, {
     routesRoot: Path.join(__dirname, 'routes'),
-    viewsRoot: Path.join(__dirname, '../views'),
+    viewsRoot: Path.join(__dirname, '../../views'),
     viewsExtension: '.hbs'
 });
 
-/*
-
-const content = fs.readFileSync('./sitecontent/html/index.html', {encoding: 'utf-8'});
-app.get('/authorize/:token', (req:Request, res:Response) => {
-
-      var token = req.param('token');
-
-    if(authDB.getUser(token)){
-        let user = authDB.findUserByToken(token);
-        res.status(200)
-        res.send('Hello ' + user.getName())        
-    }
-    else {
-        res.status(500)
-        res.send('Your Token might have expired, Kindly try again.')
-    }
-});
-*/
 
 app.listen(process.env.port || process.env.PORT || 8080,  () => {
    console.log(`Listening on port ${app.name}...`);

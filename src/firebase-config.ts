2fae13f56fabeeeddfb3bcc165941aec709ea2a0
meowcoder <meowcoder@users.noreply.github.com>
2016-08-21 02:12:59 +0500

    Add service-worker Firebase credentials

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -1,6 +1,13 @@
+import * as path from 'path';
+
+
 export const config = {
     apiKey: 'AIzaSyBhgvYzhzEpDrI2Uy4mj10VHd6I9Arze88',
     authDomain: 'meow-59086.firebaseapp.com',
     databaseURL: 'https://meow-59086.firebaseio.com',
     storageBucket: 'meow-59086.appspot.com',
+    serviceAccount: path.resolve(__dirname, '..', 'config', 'firebase.credentials.json'),
+    databaseAuthVariableOverride: {
+        uid: 'service-worker'
+    }
 };

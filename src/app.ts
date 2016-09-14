e22d81e5bc88f81cb8e8e1d25ec55622acb6efef
Nauman Qazi <nauman.qazi@systematicbytes.com>
2016-09-14 21:25:46 +1000

    production keys changed

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,8 @@ import {Firebase} from './firebase';
 import {logger} from './logger';
 import { generate_token } from './utils';
 import * as gmail from './gmail';
+import { Luis,AppCredential } from './app.config';
+
 
 const local = process.env.LOCAL_ENV || false;
 
@@ -57,22 +59,14 @@ server.get('/__/auth/handler', async function (request, response, next) {
         
     }
 
-    /*
-    response.header("Content-Type", "text/html");
-    response.write(`
-        code: ${query.code}<br>
-        uid: ${query.state}<br>
-        <pre>${JSON.stringify(tokens, null, 4)}</pre>
-    `);
-    */
     next();
 });
 
 
 // Create chat bot
 let connector = new builder.ChatConnector({
-    appId: local ? process.env.MICROSOFT_APP_ID : 'd8f8c4ab-7087-4af7-91b0-2a60571006c2',
-    appPassword: local ? process.env.MICROSOFT_APP_PASSWORD : '9SoJSjMt9fSuGVEcDheCptw',
+    appId: local ? process.env.MICROSOFT_APP_ID : AppCredential.app_id,
+    appPassword: local ? process.env.MICROSOFT_APP_PASSWORD : AppCredential.app_pass,
 });
 
 let bot = new builder.UniversalBot(connector);
@@ -80,14 +74,8 @@ let bot = new builder.UniversalBot(connector);
 
 server.post('/api/messages', connector.listen());
 
-// Create LUIS recognizer that points at our model and add it as the root '/' dialog for our Cortana Bot.
-const luis = {
-    id: '001644e2-8254-45af-9d7e-a42416bb61fd',
-    key: '2b826a013e3b4ddcac6330141e188d35',
-    url: 'https://api.projectoxford.ai/luis/v1/application',
-};
-
-const model = `${luis.url}?id=${luis.id}&subscription-key=${luis.key}`;
+// Create LUIS recognizer that points at our model and add it as the root '/' dialog for our Cortana Bot
+const model = `${Luis.url}?id=${Luis.id}&subscription-key=${Luis.key}`;
 let recognizer = new builder.LuisRecognizer(model);
 let dialog = new builder.IntentDialog({ recognizers: [recognizer] });
 
